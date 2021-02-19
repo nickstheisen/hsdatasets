79d6127b00595458c357b87d680d76c9236e9dd7
Nick Theisen <nicktheisen@gmx.de>
2021-02-19 17:49:25 +0100

    Add docstrings for documentation.

--- hsdatasets/remotesensing.py
+++ hsdatasets/remotesensing.py
@@ -85,6 +85,63 @@ DATASETS_CONFIG = {
 }
 
 class HyperspectralDataset(Dataset):
+    """
+    Base class to represent hyperspectral data sets.
+
+    Attributes
+    ----------
+    root_dir : PosixPath
+        Root directory where data sets are stored in.
+    scene : str
+        Internal short name of data set. Used as name for directory where data set is stored in.
+    transform : functional
+        Transformations applied to data samples.
+    apply_pca : bool
+        Defines if dimensionality is reduced with PCA.
+    pca_dim : int
+        Number of dimensions that data is reduced to.
+    rm_zeros_labels : bool, optional
+            All data with class-ID 0 is removed. Other class-IDs are decremented.
+    window_size : int
+        Sidelength of patches that are sampled from hyperspectral image.
+    padding_mode : str or function
+        Defines how data is padded (See numpy.pad) for further information.
+    padding_values : sequence or scalar, optional
+        Used with 'constant' padding mode. The values to set the padded values for each axis
+        (default=0).
+
+    secure_sampling : True
+        Use secure sampling. No overlap between test and train data.
+    samples : ndarray
+        List of paths to data samples.
+    pca : object
+        Stores sklearn.decomposition.PCA object that was used to reduce dimensionality
+    filepath_data : PosixPath
+        Path to hyperspectral data.
+    filepath_lables : PosixPath
+        Path to labels corresponding to hyperspectral pixels.
+
+    Methods
+    -------
+
+    __len__():
+        Return length of data set.
+
+    __getitem__(self, idx):
+        Return sample in data set at position 'idx'.
+
+    _load_data(self):
+        Downloads data and labels if not existing.
+
+    _apply_pca(self, data):
+        Applies PCA to reduce datas spectral dimensionality.
+
+     _sample_patches(self, data, labels):
+        Samples a data patch at each pixel. 
+
+    _secure_sample_patches(self, data, labels):
+        Samples a data patch at each position without overlap between test and training data.
+    """
     def __init__(self, scene, root_dir='~/data',
                  window_size=1,
                  apply_pca=False,
@@ -94,6 +151,36 @@ class HyperspectralDataset(Dataset):
                  padding_values=0,
                  secure_sampling=True,
                  transform=None):
+        """
+        Instantiates hyperspectral dataset class.
+
+        During instantiation the dataset is downloaded if necessary and data samples are 
+        extracted and stored on in persistent memory as defined by 'root_dir' and 'scene'.
+
+        Parameters
+        ----------
+        scene : str
+            Internal short name of data set. Used as name for directory where data set is stored in.
+        root_dir : PosixPath, optional
+            Root directory where data sets are stored in (default='~/data').
+        window_size : int, optional
+            Sidelength of patches that are sampled from hyperspectral image (default=1).
+        apply_pca : bool, optional
+            Defines if dimensionality is reduced with PCA (default=False).
+        pca_dim : int, optional
+            Number of dimensions that data is reduced to (default=75).
+        rm_zeros_labels : bool, optional
+            All data with class-ID 0 is removed. Other class-IDs are decremented (default=True).
+        padding_mode : str or function, optional
+            Defines how data is padded (See numpy.pad) for further information (default='constant').
+        padding_values : sequence or scalar, optional
+            Used with 'constant' padding mode. The values to set the padded values for each axis
+            (default=0).
+        secure_sampling : True, optional
+            Use secure sampling. No overlap between test and train data (default=True).
+        transform : functional, optional
+            Transformations applied to data samples. (default='None')
+        """
 
         # dataset config
         self.root_dir = Path(root_dir).expanduser().joinpath(scene)
@@ -130,9 +217,22 @@ class HyperspectralDataset(Dataset):
                     self.padding_values)
 
     def __len__(self):
+        """ Return length of data set."""
         return self.samples.shape[0]
 
     def __getitem__(self, idx):
+        """ 
+        Return sample in data set at position 'idx'.
+
+        Data patch and corresponding are loaded from hard disk. The path to it is taken
+        from 'self.samples' list. Transformations defined by 'self.transform' are applied 
+        before returning the samples.
+
+        Attributes:
+        -----------
+        idx : int
+            Position of data in sample list.
+        """
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
@@ -149,6 +249,12 @@ class HyperspectralDataset(Dataset):
         return sample
 
     def _load_data(self):
+        """
+        Downloads data and labels if not existing.
+
+        Creates directory and parent directories <root_dir>/<scene>. 
+        Data set and label image is downloaded into those directories.
+        """
         self.root_dir.mkdir(parents=True, exist_ok=True)
         self.filepath_data = self.root_dir.joinpath(DATASETS_CONFIG[self.scene]['img']['name'])
         self.filepath_labels = self.root_dir.joinpath(DATASETS_CONFIG[self.scene]['gt']['name'])
@@ -166,6 +272,12 @@ class HyperspectralDataset(Dataset):
                 urlretrieve(url, filename=self.filepath_labels, reporthook=t.update_to)
 
     def _apply_pca(self, data):
+        """
+        Applies PCA to reduce datas spectral dimensionality.
+
+        sklearn.decomposition.PCA is applied to data to reduce dimensionality. 
+        The object is stored in member variable to use e.g. for inverse transformation.
+        """
         X = np.reshape(data, (-1, data.shape[2]))
         self.pca = PCA(n_components=self.pca_dim, whiten=True)
         X = self.pca.fit_transform(X)
@@ -173,6 +285,28 @@ class HyperspectralDataset(Dataset):
         return X
 
     def _sample_patches(self, data, labels):
+        """
+        Samples a data patch at each pixel. 
+        
+        The hyperspectral data cube is padded along its spatial dimensions. Then
+        a sample patch is extracted at each position. Each patch is stored as a mat-file
+        together with its corresponding label in <root_dir>/<scene>/patches/. The data paths are
+        stored in the same directory with name 'sample_list.txt'.
+
+        If sampled data already exists their paths are simply imported from 'sample_list.txt'-file.
+        To resample delete <root_dir>/<scene>/patches or move to another location.
+
+        If 'self.rm_zero_labels' is true, data with zero labels are ignored and all labels >=1 are 
+        decremented.
+
+        Attributes
+        ----------
+
+        data : ndarray
+            Hyperspectral image from which patches are sampled.
+        labels : ndarray
+            Label image corresponding to hyperspectral image cube.
+        """
         patchdir = self.root_dir.joinpath('patches')
         if patchdir.is_dir():
             warnings.warn('Sampled data already exists remove directory'
@@ -216,6 +350,9 @@ class HyperspectralDataset(Dataset):
         np.savetxt(patchdir.joinpath('sample_list.txt'), self.samples, fmt="%s")
 
     def _secure_sample_patches(self, data, labels):
+        """
+        Samples a data patch at each position without overlap between test and training data.
+        """
         warnings.warn('Secure sampling is not implemented yet! Falling back to normal sampling.')
         return self._sample_patches(data, labels)
 
